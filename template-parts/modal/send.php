66c5502b1519a883ea78b25ca182e1eab25111f6
heymynameismilk <heymynameismilk@gmail.com>
2020-01-16 21:05:04 +0700

    upd

---
+++ template-parts/modal/send.php
@@ -0,0 +1,16 @@
+<div class="modal hidden">
+    <div class="modal__box">
+        <h1 class="modal__title">Оставьте заявку</h1>
+        <h2 class="modal__subtitle">Консультант свяжется с вами в течение 30 секунд</h2>
+        <label>Ваше имя
+            <input required class="modal__input name">
+        </label>
+        <label>Ваш номер телефона
+            <input required class="modal__input number" placeholder="+7 (___)___-__-__">
+        </label>
+        <label>Сообщение
+            <textarea class="modal__input"></textarea>
+        </label>
+        <button class="button button--brown">Получить консультацию</button>
+    </div>
+</div>
